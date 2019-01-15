7224ccc69ad6ba4e707a3ce13f54a797244a55b7
hohohohyun <hhhwang94@naver.com>
2019-01-15 17:50:07 +0900

    second commit

--- webboard/board/models.py
+++ webboard/board/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.db.utils import timezone
+from django.utils import timezone
 
 # Create your models here.
 
 class Post(models.Model):
-    adthor = models.Foreignkey('auth.User',on_delete=models.CASCADE)
+    adthor = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200),
     text = models.TextField()
     created_date = models.DateTimeField(
